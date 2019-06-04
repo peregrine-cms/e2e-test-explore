692dc5018a7f02397b41bc6012378a2407d04e29
devin tuffy <devintuffy@gmail.com>
2019-06-04 13:24:37 -0700

    Error out and let user know if docker is unavailable or unable to start; update test runner so user can select which tagged instance on docker hub to use

--- scripts/startDocker.sh
+++ scripts/startDocker.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+#
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: docker is not installed.' >&2
+  exit 1
+fi
+
 # Start docker with the first argument as the container name; if none is specified docker will
 # ignore the --name option and just pick one
 
-docker run -d --rm -p 8080:8080 --name=$1 reusr1/peregrine-cms:develop
+TAG=${2:-develop}
+
+docker run -d --rm -p 8080:8080 --name=$1 reusr1/peregrine-cms:$TAG
+
+# make sure it didn't fail for whatever reason
+if ! [ $? -eq 0 ]; then
+    echo 'Error: docker unable to start.' >&2
+    exit 1
+fi
 
 # here's where we'll spam "docker inspect" to wait for the health check to return true
 # until that's ready, we'll just sleep for 15 seconds :O
